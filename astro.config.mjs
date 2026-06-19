71b1cd845295c539a8aeed45aecf373e8484f88e
kingtiger999 <3031918699@qq.com>
2026-06-19 11:43:36 +0800

    配置github pages部署与base路径

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,9 +52,9 @@ const adapter = process.env.CF_WORKERS
 
 // https://astro.build/config
 export default defineConfig({
-	site: siteConfig.site_url,
+	site: "https://kingtiger999.github.io/",
 
-	base: "/",
+	base: "/ai_infa_pages/",
 	trailingSlash: "always",
 
 	adapter,
